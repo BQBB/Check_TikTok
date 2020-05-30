03f97820b823b0baaf6e9f743d923d469ba18e5e
BQBB <49294401+BQBB@users.noreply.github.com>
2020-05-30 18:37:39 +0300

    Add files via upload

---
+++ Check-TikTok/Check-TikTok/Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.Green
+        Me.TextBox1.Location = New System.Drawing.Point(0, 14)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(122, 136)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Black
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.ForeColor = System.Drawing.Color.Red
+        Me.TextBox2.Location = New System.Drawing.Point(123, 14)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(122, 136)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(0, 214)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(245, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.Black
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox3.ForeColor = System.Drawing.Color.Green
+        Me.TextBox3.Location = New System.Drawing.Point(0, 151)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(122, 20)
+        Me.TextBox3.TabIndex = 3
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.Color.Black
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox4.ForeColor = System.Drawing.Color.Red
+        Me.TextBox4.Location = New System.Drawing.Point(123, 151)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(122, 20)
+        Me.TextBox4.TabIndex = 4
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BackColor = System.Drawing.Color.Black
+        Me.TextBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox5.ForeColor = System.Drawing.Color.Green
+        Me.TextBox5.Location = New System.Drawing.Point(0, 172)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(245, 20)
+        Me.TextBox5.TabIndex = 5
+        Me.TextBox5.Text = "50"
+        Me.TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox6
+        '
+        Me.TextBox6.BackColor = System.Drawing.Color.Black
+        Me.TextBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox6.ForeColor = System.Drawing.Color.Green
+        Me.TextBox6.Location = New System.Drawing.Point(0, 193)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(245, 20)
+        Me.TextBox6.TabIndex = 6
+        Me.TextBox6.Text = "8"
+        Me.TextBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Red
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(1, -7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(266, 20)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Check - TikTok"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Green
+        Me.Label2.Location = New System.Drawing.Point(2, 175)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Speed"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Green
+        Me.Label3.Location = New System.Drawing.Point(1, 196)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Sleep"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.Green
+        Me.Label4.Location = New System.Drawing.Point(200, 196)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Second"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Red
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(230, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(14, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "X"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(245, 238)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.ForeColor = System.Drawing.Color.Red
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Check - TikTok"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Check-TikTok/Check-TikTok/Form1.vb
@@ -0,0 +1,121 @@
+﻿Imports System.Text, System.IO, System.Threading, System.Net, System.Net.Mail
+Public Class Form1
+    Dim lis As New ListBox
+    Dim re As String()
+    Dim tru As Integer = 0
+    Dim fls As Integer = 0
+    Dim point As New Point()
+    Dim boolean0 As Boolean
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        MsgBox("This Project Coded By > Karar" + vbCrLf + "Instagram : BQBB" + vbCrLf + "Telegram : camera", True, "CopyRight")
+        CheckForIllegalCrossThreadCalls = False
+
+        re = File.ReadAllLines("list.txt")
+        For i As Integer = 0 To re.Length - 1
+            lis.Items.Add(re(i))
+        Next
+
+    End Sub
+    Sub one()
+        For i As Integer = 1 To Convert.ToInt32(TextBox5.Text)
+            Dim thread As Thread = New Thread(AddressOf two)
+            thread.Start(i)
+        Next
+    End Sub
+    Sub two(i As Integer)
+        Do
+            Try
+                Dim str As New StringBuilder()
+                str.Append(lis.Items(i))
+
+                lis.Items.RemoveAt(i)
+                check(str.ToString())
+            Catch ex As Exception
+
+                Exit Sub
+            End Try
+        Loop
+    End Sub
+    Function check(user As String)
+        Try
+            If IsNumeric(user(0)) Then
+                '' Pass
+                TextBox2.AppendText(user + vbCrLf)
+                fls += 1
+                TextBox4.Text = fls.ToString()
+            Else
+                Dim req As HttpWebRequest = DirectCast(WebRequest.Create("https://www.tiktok.com/@" + user), HttpWebRequest)
+                req.Method = "get"
+                req.UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13F69 Tiktok 8.4.0 (iPhone7,2; iPhone OS 9_3_2; nb_NO; nb-NO; scale=2.00; 750x1334"
+                req.CookieContainer = New CookieContainer
+                Dim ris As HttpWebResponse = DirectCast(req.GetResponse(), HttpWebResponse)
+                Dim strea As Stream = ris.GetResponseStream()
+                Dim rdr As New StreamReader(strea)
+                Dim result As String = rdr.ReadToEnd()
+                If result.Contains("{""id"":") Then
+                    TextBox2.AppendText(user + vbCrLf)
+                    fls += 1
+                    TextBox4.Text = fls.ToString()
+                Else
+                    save(user)
+                    TextBox1.AppendText(user + vbCrLf)
+                    tru += 1
+                    TextBox3.Text = tru.ToString()
+                End If
+                Thread.Sleep(Convert.ToInt32(TextBox6.Text) * 1000)
+
+
+            End If
+
+
+        Catch ex As System.ObjectDisposedException
+
+            End
+        End Try
+    End Function
+    Sub save(text As String)
+        Dim save As StreamWriter = File.AppendText("Available.txt")
+        save.WriteLine(text)
+        save.Close()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        one()
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+        End
+    End Sub
+
+
+
+
+
+
+
+    Private Sub Label1_MouseDown(sender As Object, e As MouseEventArgs) Handles Label1.MouseDown
+        If e.Button = MouseButtons.Left Then
+            Me.point = New Point((0 - e.X), (0 - e.Y))
+            Me.boolean0 = True
+
+        End If
+    End Sub
+
+    Private Sub Label1_MouseMove(sender As Object, e As MouseEventArgs) Handles Label1.MouseMove
+        If Me.boolean0 Then
+            Dim mousePosition As Point = Control.MousePosition
+            mousePosition.Offset(Me.point.X, Me.point.Y)
+            Me.Location = mousePosition
+
+        End If
+    End Sub
+
+    Private Sub Label1_MouseUp(sender As Object, e As MouseEventArgs) Handles Label1.MouseUp
+        If e.Button = MouseButtons.Left Then
+            Me.boolean0 = False
+        End If
+    End Sub
+
+
+End Class
